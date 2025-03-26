4fab7960903ae295af4927df92f4de2039f5e8dc
scv1702 <scv1702@naver.com>
2025-03-26 23:37:26 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/pipy/PipyApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package pipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipyApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipyApplicationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pipy/PipyApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pipy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipyApplicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
