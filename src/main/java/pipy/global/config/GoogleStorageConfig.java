34346e1741e4c37eac49b93bdbb4fea2c2be139d
scv1702 <scv1702@naver.com>
2025-05-01 16:11:29 +0900

    PIPY-50 chore: CD 파이프라인에 GCP 키 설정 추가

--- src/main/java/pipy/global/config/GoogleStorageConfig.java
+++ src/main/java/pipy/global/config/GoogleStorageConfig.java
@@ -3,6 +3,7 @@ package pipy.global.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.util.ResourceUtils;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class GoogleStorageConfig {
 
@@ -19,6 +21,7 @@ public class GoogleStorageConfig {
 
     @Bean
     public Storage storage() throws IOException {
+        log.info("Google Storage credentials path: {}", credentialsPath);
         final InputStream keyFile = ResourceUtils.getURL(credentialsPath)
             .openStream();
         return StorageOptions.newBuilder()
