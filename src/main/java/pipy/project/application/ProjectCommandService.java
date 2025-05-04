ddc74eda00c430e22fed9a83c2f3803bf2b71e42
scv1702 <scv1702@naver.com>
2025-05-04 17:59:14 +0900

    PIPY-51 feat: 프로젝트 썸내일 갱신 비동기 처리

--- src/main/java/pipy/project/application/ProjectCommandService.java
+++ src/main/java/pipy/project/application/ProjectCommandService.java
@@ -6,14 +6,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import pipy.member.domain.Member;
-import pipy.node.application.ImageSaveCommand;
-import pipy.node.application.ImageSaver;
 import pipy.project.domain.Project;
 import pipy.project.domain.ProjectReader;
 import pipy.project.domain.ProjectWriter;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +20,7 @@ public class ProjectCommandService {
     private final ProjectReader projectReader;
     private final ProjectWriter projectWriter;
 
-    private final ImageSaver imageSaver;
+    private final ProjectThumbnailSaver projectThumbnailSaver;
 
     @Transactional
     public Project createProject(final CreateProjectCommand command) {
@@ -44,30 +41,21 @@ public class ProjectCommandService {
         project.updateCanvas(canvas);
     }
 
-    @Transactional
-    public void updateThumbnail(final Long projectId, final MultipartFile thumbnail) {
-        final Project project = projectReader.readById(projectId);
-
+    public void updateThumbnail(
+        final Long projectId,
+        final MultipartFile thumbnail
+    ) {
         final String ext = thumbnail.getContentType();
         if (ext == null || !ext.startsWith("image/")) {
             throw new IllegalArgumentException("Invalid image type");
         }
 
-        final Long memberId = project.getOwner().getId();
-        final String uuid = UUID.randomUUID().toString();
-        final String filename = String.format("%d/projects/%d/%s", memberId, projectId, uuid);
+        final Project project = projectReader.readById(projectId);
 
         try {
-            final byte[] thumbnailBytes = thumbnail.getInputStream().readAllBytes();
-            final ImageSaveCommand command = new ImageSaveCommand(
-                thumbnail.getContentType(),
-                filename,
-                thumbnailBytes
-            );
-            final String thumbnailUrl = imageSaver.save(command);
-            project.updateThumbnail(thumbnailUrl);
+            projectThumbnailSaver.save(project, thumbnail.getBytes());
         } catch (final IOException exception) {
-            throw new RuntimeException("Failed to upload thumbnail", exception);
+            throw new IllegalArgumentException("Failed to read image", exception);
         }
     }
 }
