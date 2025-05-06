e0713156b2896ad5d1d8fe9a03804636c3c430bc
scv1702 <scv1702@naver.com>
2025-05-07 02:13:55 +0900

    fix: 프로젝트 썸내일 갱신에서 이전 썸내일 삭제 시 경로 오류 수정

--- src/main/java/pipy/node/infrastructure/GoogleCloudStorageManager.java
+++ src/main/java/pipy/node/infrastructure/GoogleCloudStorageManager.java
@@ -3,11 +3,13 @@ package pipy.node.infrastructure;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pipy.node.application.StorageManager;
 import pipy.node.application.StorageSaveCommand;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class GoogleCloudStorageManager implements StorageManager {
@@ -29,12 +31,16 @@ public class GoogleCloudStorageManager implements StorageManager {
             blobInfo,
             command.bytes()
         );
+        final String objectName = blobInfo.getBlobId().getName();
+        log.debug("File saved: {}", objectName);
         return String.format("%s/%s/%s", storageUrl, bucketName, command.filename());
     }
 
     @Override
     public void delete(final String filename) {
-        final String objectName = filename.replace(storageUrl + "/", "");
+        final String prefix = String.format("%s/%s/", storageUrl, bucketName);
+        final String objectName = filename.replace(prefix, "");
+        log.debug("Deleting file: {}", objectName);
         final BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, objectName).build();
         storage.delete(blobInfo.getBlobId());
     }
